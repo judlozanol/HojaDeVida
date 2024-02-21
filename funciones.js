b21fa79912ab822aa41c72712a464b738b38d573
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2024-02-21 09:38:33 -0500

    bases para formulario + funcion añadir botones

---
+++ funciones.js
@@ -0,0 +1,23 @@
+function agregarNacimiento(){
+    var fecha = document.createElement("input");
+    var lugar= document.createElement("input");
+    var textFecha= document.createElement("li");
+    var textLugar= document.createElement("li");
+
+    textFecha.textContent="Fecha de Nacimiento:";
+    textLugar.textContent="Lugar de Nacimiento:";
+    fecha.type = "date";
+    fecha.name = "fechaNacimiento";
+    fecha.placeholder = "Ingrese su fecha de nacimiento";
+    lugar.type = "text"
+    lugar.name ="lugarNacimiento"
+    lugar.placeholder = "Ingrese su lugar de nacimiento";
+
+    document.getElementById("elementosAdicionales").appendChild(textFecha);
+    document.getElementById("elementosAdicionales").appendChild(fecha);
+    document.getElementById("elementosAdicionales").appendChild(textLugar);
+    document.getElementById("elementosAdicionales").appendChild(lugar);
+
+    document.getElementById("agregarNacimiento").style.display ="none";
+
+}
