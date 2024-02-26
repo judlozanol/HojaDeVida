a5392e40d7a96709ae84d223e33b4a1d595e000e
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2024-02-26 08:54:15 -0500

    creacion pagina de entrega e inicio de la funcion de paso de datos

--- funciones.js
+++ funciones.js
@@ -1,25 +1,76 @@
-function agregarNacimiento(){
-    var fecha = document.createElement("input");
-    var lugar= document.createElement("input");
-    var textFecha= document.createElement("li");
-    var textLugar= document.createElement("li");
-    var form=document.getElementById("formHojaVida");
 
-    textFecha.textContent="Fecha de Nacimiento:";
-    textLugar.textContent="Lugar de Nacimiento:";
-    fecha.type = "date";
-    fecha.name = "fechaNacimiento";
-    fecha.placeholder = "Ingrese su fecha de nacimiento";
-    lugar.type = "text"
-    lugar.name ="lugarNacimiento"
-    lugar.placeholder = "Ingrese su lugar de nacimiento";
+function leerGET(){ 
+    
+    var cadGET = location.search.substr(1,location.search.length); 
+    //rompe la cadena para separar pares variable=valor
+    var arrGET = cadGET.split("&"); 
+    var asocGET = new Array(); 
+    var variable = ""; 
+    var valor = ""; 
+    //llena el arreglo asociativo
+    for(i=0;i<arrGET.length;i++){ 
+        var aux = arrGET[i].split("="); 
+        variable = aux[0];
+        valor = aux[1];
+        valor =valor.replace("+"," ")
+        asocGET[variable] = valor; 
+    } 
+    return asocGET; 
+}
+function escribir(){
+    var paresVarValor = leerGET();
+    var keys= Object.keys(paresVarValor);
+    for(var key in keys){
+        var nuevoParrafo = document.createElement('p');
+        // Crear un nodo de texto para el párrafo
+        var texto = document.createTextNode(paresVarValor["correo"]);
 
-    document.getElementById("elementosAdicionales").appendChild(textFecha);
-    document.getElementById("elementosAdicionales").appendChild(fecha);
-    document.getElementById("elementosAdicionales").appendChild(textLugar);
-    document.getElementById("elementosAdicionales").appendChild(lugar);
+        // Agregar el nodo de texto al párrafo
+        nuevoParrafo.appendChild(texto);
 
-    document.getElementById("agregarNacimiento").style.display ="none";
+        // Obtener una referencia al contenedor div
+        var contenedor = document.getElementById('perfil');
 
-    /* Falta agregar los elementos del div al formulario  */
+        // Agregar el párrafo como hijo del contenedor div
+        contenedor.appendChild(nuevoParrafo);
+    }
+    //revisar esta funcion 
+}
+
+function validarCampos(){
+    if(document.getElementById("nombre").value==""){
+        alert("El nombre no puede ser vacio.");
+        document.getElementById("formHojaVida").action="captura.html";
+        return;
+    }
+    if(document.getElementById("apellido").value==""){
+        alert("El apellido no puede ser vacio.");
+        document.getElementById("formHojaVida").action="captura.html";
+        return;
+    }
+    if(document.getElementById("residencia").value==""){
+        alert("El lugar de residencia no puede ser vacio.");
+        document.getElementById("formHojaVida").action="captura.html";
+        return;
+    }
+    if(document.getElementById("correo").value==""){
+        alert("El correo no puede ser vacio.");
+        document.getElementById("formHojaVida").action="captura.html";
+        return;
+    }
+    if(document.getElementById("telCelular").value==""){
+        alert("Ingrese un número de celular.");
+        document.getElementById("formHojaVida").action="captura.html";
+        return;
+    }
+    if(document.getElementById("perfil").value==""){
+        alert("El perfil no puede ser vacio.");
+        document.getElementById("formHojaVida").action="captura.html";
+        return;
+    } 
+    if(document.getElementById("habilidades").value==""){
+        alert("Ingrese al menos una habilidad.");
+        document.getElementById("formHojaVida").action="captura.html";
+        return;
+    }alert("Formulario Enviado");
 }
